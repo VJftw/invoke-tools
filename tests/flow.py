eb956a6f39bdbe568de8a2e2d99dc6e023765483
VJ Patel <meetthevj@gmail.com>
2016-05-25 01:09:33 +0100

    refactored to use mock.patch correctly

--- tests/flow.py
+++ tests/flow.py
@@ -5,13 +5,14 @@ Flow tests
 
 import unittest
 import mock
+import os
 
-import sys
-
-utils_mock = mock.Mock()
-utils_mock.Utils.get_branch.return_value = "master"
-utils_mock.Utils.get_version.return_value = "1.0.0"
-sys.modules['idflow.utils'] = utils_mock
+# import sys
+#
+# utils_mock = mock.Mock()
+# utils_mock.Utils.get_branch.return_value = "master"
+# utils_mock.Utils.get_version.return_value = "1.0.0"
+# sys.modules['idflow.utils'] = utils_mock
 
 from idflow import Flow
 
@@ -20,6 +21,20 @@ class FlowTests(unittest.TestCase):
     """
     Tests for Flow
     """
+
+    def setUp(self):
+        self.os_environ = os.environ.copy()
+
+    def tearDown(self):
+        os.environ = self.os_environ
+
+    def __se_check_output(self, cmds):
+        if cmds == "git describe --tags".split(" "):
+            return "1.0.0".encode('utf-8')
+        elif cmds == "git rev-parse --short HEAD".split(" "):
+            return "fe5a7e3".encode('utf-8')
+        elif cmds == "git rev-parse --abbrev-ref HEAD".split(" "):
+            return "develop".encode('utf-8')
 
     def test_init(self):
         """
@@ -44,102 +59,112 @@ class FlowTests(unittest.TestCase):
         """
         Flow: Should return the development container name
         """
-        flow = Flow(
-            "vjftw/invoke-docker-flow"
-        )
-        self.assertEqual(
-            flow.get_development_container_name(),
-            "vjftw/invoke-docker-flow:master-dev"
-        )
-        flow = Flow(
-            "vjftw/invoke-docker-flow",
-            "tool"
-        )
-        self.assertEqual(
-            flow.get_development_container_name(),
-            "vjftw/invoke-docker-flow:tool-master-dev"
-        )
+        with mock.patch('idflow.utils.check_output',
+                        side_effect=self.__se_check_output):
+            flow = Flow(
+                "vjftw/invoke-docker-flow"
+            )
+            self.assertEqual(
+                flow.get_development_container_name(),
+                "vjftw/invoke-docker-flow:develop-dev"
+            )
+            flow = Flow(
+                "vjftw/invoke-docker-flow",
+                "tool"
+            )
+            self.assertEqual(
+                flow.get_development_container_name(),
+                "vjftw/invoke-docker-flow:tool-develop-dev"
+            )
 
     def test_get_build_container_name(self):
         """
         Flow: Should return the build container name
         """
-        flow = Flow(
-            "vjftw/invoke-docker-flow"
-        )
-        self.assertEqual(
-            flow.get_build_container_name(),
-            "vjftw/invoke-docker-flow:master-1.0.0"
-        )
+        with mock.patch('idflow.utils.check_output',
+                        side_effect=self.__se_check_output):
+            flow = Flow(
+                "vjftw/invoke-docker-flow"
+            )
+            self.assertEqual(
+                flow.get_build_container_name(),
+                "vjftw/invoke-docker-flow:develop-1.0.0"
+            )
 
-        flow = Flow(
-            "vjftw/invoke-docker-flow",
-            "tool"
-        )
-        self.assertEqual(
-            flow.get_build_container_name(),
-            "vjftw/invoke-docker-flow:tool-master-1.0.0"
-        )
+            flow = Flow(
+                "vjftw/invoke-docker-flow",
+                "tool"
+            )
+            self.assertEqual(
+                flow.get_build_container_name(),
+                "vjftw/invoke-docker-flow:tool-develop-1.0.0"
+            )
 
     def test_get_build_container_tag(self):
         """
         Flow: Should return the build container tag
         """
-        flow = Flow(
-            "vjftw/invoke-docker-flow"
-        )
-        self.assertEqual(
-            flow.get_build_container_tag(),
-            "master-1.0.0"
-        )
+        with mock.patch('idflow.utils.check_output',
+                        side_effect=self.__se_check_output):
+            flow = Flow(
+                "vjftw/invoke-docker-flow"
+            )
+            self.assertEqual(
+                flow.get_build_container_tag(),
+                "develop-1.0.0"
+            )
 
-        flow = Flow(
-            "vjftw/invoke-docker-flow",
-            "tool"
-        )
-        self.assertEqual(
-            flow.get_build_container_tag(),
-            "tool-master-1.0.0"
-        )
+            flow = Flow(
+                "vjftw/invoke-docker-flow",
+                "tool"
+            )
+            self.assertEqual(
+                flow.get_build_container_tag(),
+                "tool-develop-1.0.0"
+            )
 
     def test_get_branch_container_name(self):
         """
         Flow: Should return the branch container name
         """
-        flow = Flow(
-            "vjftw/invoke-docker-flow"
-        )
-        self.assertEqual(
-            flow.get_branch_container_name(),
-            "vjftw/invoke-docker-flow:master"
-        )
+        with mock.patch('idflow.utils.check_output',
+                        side_effect=self.__se_check_output):
+            flow = Flow(
+                "vjftw/invoke-docker-flow"
+            )
+            self.assertEqual(
+                flow.get_branch_container_name(),
+                "vjftw/invoke-docker-flow:develop"
+            )
 
-        flow = Flow(
-            "vjftw/invoke-docker-flow",
-            "tool"
-        )
-        self.assertEqual(
-            flow.get_branch_container_name(),
-            "vjftw/invoke-docker-flow:tool-master"
-        )
+            flow = Flow(
+                "vjftw/invoke-docker-flow",
+                "tool"
+            )
+            self.assertEqual(
+                flow.get_branch_container_name(),
+                "vjftw/invoke-docker-flow:tool-develop"
+            )
 
     def test_get_branch_container_tag(self):
         """
         Flow: Should return the branch container tag
         """
-        flow = Flow(
-            "vjftw/invoke-docker-flow"
-        )
-        self.assertEqual(
-            flow.get_branch_container_tag(),
-            "master"
-        )
+        with mock.patch('idflow.utils.check_output',
+                        side_effect=self.__se_check_output):
+            flow = Flow(
+                "vjftw/invoke-docker-flow"
+            )
+            self.assertEqual(
+                flow.get_branch_container_tag(),
+                "develop"
+            )
 
-        flow = Flow(
-            "vjftw/invoke-docker-flow",
-            "tool"
-        )
-        self.assertEqual(
-            flow.get_branch_container_tag(),
-            "tool-master"
-        )
+            flow = Flow(
+                "vjftw/invoke-docker-flow",
+                "tool"
+            )
+            self.assertEqual(
+                flow.get_branch_container_tag(),
+                "tool-develop"
+            )
